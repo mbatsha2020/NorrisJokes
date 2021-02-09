cd02d4058e860b8329e492551e7a7f431f479e71
Lisa Mbatsha <lmbatsha16@gmail.com>
2021-02-09 10:19:17 +0200

    made a change

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ app = Flask(__name__)
 def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
-
     image_tag = "<img src="+response['icon_url']+" alt='Chuck's Image'>"
 
     return "<strong>Random joke from chuck norris: </strong>" + response['value'] + image_tag
 
 # https://api.chucknorris.io/jokes/random
-# if __name__ =='__main__':
-#     app.run()
+if __name__ =='__main__':
+    app.run()
